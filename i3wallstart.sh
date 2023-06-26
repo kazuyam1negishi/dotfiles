870b4520dc0b05fc2005303c2db7bfe027df330f
Jimmy Soto Agustin <44762021+kazuyam1negishi@users.noreply.github.com>
2023-06-25 22:04:09 -0700

    Delete i3 wallpaper script
    A method of setting the wallpaper on startup was setup in i3conf without any polybars being closed, and will be provided in the next commit.

--- i3wallstart.sh
+++
@@ -1,6 +0,0 @@
-# Remember to make this executable!
-
-# Sleep necessary for command to work properly
-sleep 1
-# Swap this for your username and directory!
-wal -i /home/kazuya/Pictures/Wallpapers/ds_17.jpg -a 50
